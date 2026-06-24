6fd2976730bcaf584fb0d3e8b44a4568784fb684
crisbardero7-art <crisbardero7@gmail.com>
2026-06-24 03:46:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // 1. Cargamos la librería para leer el archivo secreto .env
 require('dotenv').config();
+
+// === ENGAÑO PARA RENDER (ACTUALIZADO PARA CORREGIR PUERTOS) ===
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('¡Bot online!'));
+app.listen(process.env.PORT || 10000, '0.0.0.0', () => console.log('Servidor web del bot listo.'));
+// ==============================================================
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -33,19 +41,19 @@ client.on('guildMemberAdd', async (member) => {
         const newInvites = await member.guild.invites.fetch();
         const oldInvites = invitesCache.get(member.guild.id);
         
-        // Buscamos cuál link aumentó su contador de usos en la base de datos
+        // Buscamos cuál link aumentó su contador de usos
         const inviteUsed = newInvites.find(i => oldInvites && i.uses > (oldInvites.get(i.code) || 0));
         
         // === TU ID DE CANAL ===
-        // REEMPLAZA ESTO: Pon el ID de tu canal de bienvenidas manteniendo las comillas simples
-        const welcomeChannel = member.guild.channels.cache.get('1519204854197784617');
+        // REEMPLAZA ESTO: Pon aquí los números de tu canal de bienvenidas (mantén las comillas simples)
+        const welcomeChannel = member.guild.channels.cache.get('1519240503021797526');
         
         if (!welcomeChannel) return console.log("No encontré el canal de texto.");
 
         if (inviteUsed) {
             welcomeChannel.send(`¡Hola ${member}! Bienvenido al servidor. Entraste usando la invitación de **${inviteUsed.inviter.username}** (Código: \`${inviteUsed.code}\`).`);
         } else {
-            // Si ningún link subió de uso, es porque usaron la URL personalizada (Vanity URL de nivel de servidor)
+            // Si ningún link subió de uso, es porque usaron la URL personalizada (Vanity URL)
             welcomeChannel.send(`¡Hola ${member}! Bienvenido al servidor. Entraste usando la URL personalizada del servidor.`);
         }
         
